f10a987cd38f4e5f677b9c3fe1246ff11a5ef241
Aiden Wesley <116656217+wesleyatwit@users.noreply.github.com>
2023-10-03 08:12:51 -0400

    Add files via upload

---
+++ homeworkTest.java
@@ -0,0 +1,13 @@
+package q05homework;
+import java.util.Scanner;
+
+public class homeworkTest {
+
+	public static void main(String[] args) {
+		String response;
+		Scanner input = new Scanner(System.in);
+		System.out.printf("What is your favorite day of the week?");
+		response = input.next();
+	}
+
+}
